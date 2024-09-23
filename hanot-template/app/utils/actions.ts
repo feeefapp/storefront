67da3a129d73ad904f8161cd1eca370425efeac4
noahdev0 <96663091+noahdev0@users.noreply.github.com>
2024-09-24 00:40:41 +0100

    sma7li :'(

---
+++ hanot-template/app/utils/actions.ts
@@ -0,0 +1,11 @@
+"use server ";
+import { FeeeF } from "feeef";
+
+export const ff = new FeeeF({
+  apiKey: "c43Yfd3bgolijJU3b3bx095vlfTrvnL94baZrd1",
+  baseURL: "https://feeef.app/api/v1",
+  cache: 10,
+  // baseURL: "http://localhost:3333/api/v1",
+});
+
+  
